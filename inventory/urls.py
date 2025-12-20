0cc512ba648e61fe6d9d11c69aa1eea5f558138d
mehadi <mehadi214100@gmail.com>
2025-12-21 00:13:39 +0600

    templated added

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.dashboard,name="dashboard"),
+    path('stock_in/',views.stock_in,name="stock_in"),
+    path('dispense/',views.dispense,name="dispense"),
+    path('reports/',views.reports,name="reports"),
+    path('inventory/',views.inventory,name="inventory"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
 def dashboard(request):
-    pass
+    return render(request,'index.html')
+
+def stock_in(request):
+    return render(request,'stock_in.html')
+
+def dispense(request):
+    return render(request,'dispense.html')
+
+def reports(request):
+    return render(request,'reports.html')
+
+def inventory(request):
+    return render(request,'inventory.html')
